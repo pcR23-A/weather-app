de3e47bd33c9adf77d32688b16ab84d9cde30c6c
Pedro de Castro Régis <pedrohot234@gmail.com>
2024-12-23 00:29:24 -0300

    Style Changes

--- weather_forecast/views.py
+++ weather_forecast/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 import requests
 import os
 
-apiKeyWeather = os.environ["forecast-api-key"]
-apiKeyCoord = os.environ["coord-api-key"]
+apiKeyWeather = 1 #os.environ["forecast-api-key"]
+apiKeyCoord = 1 #os.environ["coord-api-key"]
 
 def index(request):
     return render(request, 'index.html')
